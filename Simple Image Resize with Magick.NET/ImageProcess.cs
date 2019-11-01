b04ab1a386133726ee794a8572ed336e8c69d5f1
Adebayo Adesegun Daniel <chamsdev@outlook.com>
2019-11-01 22:13:55 +0100

    inital commit

---
+++ Simple Image Resize with Magick.NET/ImageProcess.cs
@@ -0,0 +1,60 @@
+﻿using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Simple_Image_Resize_with_Magick.NET
+{
+    public class ImageProcess
+    {
+        private string _inputPath;
+        private string _outputPath;
+        private int _resizeToHeight;
+        private int _resizeToWidth; 
+        public ImageProcess(string inputPath, string outPutPath, int resizeHeight = 650, int resizeWidth = 600)
+        {
+            _inputPath = inputPath;
+            _outputPath = outPutPath;
+            _resizeToHeight = resizeHeight;
+            _resizeToWidth = resizeWidth;
+        }
+
+        /// <summary>
+        /// Compress Image 
+        /// </summary>
+        /// <returns></returns>
+       public Tuple<bool, string> CompressImage()
+       {
+            try
+            {
+                using (var image = new MagickImage(_inputPath))
+                {
+                    var geometry = new MagickGeometry
+                    {
+                        Height = _resizeToHeight,
+                        Width = _resizeToWidth,
+                        IgnoreAspectRatio = false,
+                        Greater = true
+                    };
+                    image.Resize(geometry);
+                    using (var stream = new MemoryStream())
+                    {
+                        image.Write(stream);
+                        byte[] imageByte = stream.ToArray();
+                        File.WriteAllBytes(_outputPath, imageByte);
+                    }
+                }
+                return new Tuple<bool, string>(true, $"Image saved to {_outputPath}");
+            }
+            catch (MagickException ex) // Catch Magick ExceptionErrors
+            {
+                throw ex;
+            }
+            catch (Exception ex) // Catch Exception Errors 
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Simple Image Resize with Magick.NET/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using ImageMagick;
+
+namespace Simple_Image_Resize_with_Magick.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImageProcess imageProcess = new ImageProcess(@"C:\Users\Daniel\Pictures\IMG_20190212_125706_0.jpg", @"C:\Users\Daniel\Pictures\newConverted.jpg");
+            var result = imageProcess.CompressImage();
+            if(result.Item1)
+            {
+                Console.WriteLine(result.Item2);
+            }
+        }
+    }
+}
