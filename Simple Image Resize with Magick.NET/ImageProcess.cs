994eda4464be51aae4b7532d80d06e3a7ecb629f
Adesegun Adebayo Daniel <adesegun.adebayo@allianz.com>
2020-04-08 19:26:47 +0100

    Update repo to add method to save from stream

--- Simple Image Resize with Magick.NET/ImageProcess.cs
+++ Simple Image Resize with Magick.NET/ImageProcess.cs
@@ -11,7 +11,16 @@ namespace Simple_Image_Resize_with_Magick.NET
         private string _inputPath;
         private string _outputPath;
         private int _resizeToHeight;
-        private int _resizeToWidth; 
+        private int _resizeToWidth;
+        private Stream _inputStream;
+
+        public ImageProcess(Stream inputStream, string outputPath, int resizeHeight, int resizeWidth = 650)
+        {
+            _inputStream = inputStream;
+            _outputPath = outputPath;
+            _resizeToHeight = resizeHeight;
+            _resizeToWidth = resizeWidth;
+        }
         public ImageProcess(string inputPath, string outPutPath, int resizeHeight = 650, int resizeWidth = 600)
         {
             _inputPath = inputPath;
@@ -24,7 +33,7 @@ namespace Simple_Image_Resize_with_Magick.NET
         /// Compress Image 
         /// </summary>
         /// <returns></returns>
-       public Tuple<bool, string> CompressImage()
+       public Tuple<bool, string> Compress_Save_From_FilePath()
        {
             try
             {
@@ -56,5 +65,40 @@ namespace Simple_Image_Resize_with_Magick.NET
                 throw ex;
             }
         }
+
+
+       public Tuple<bool, string> Compress_Save_From_Stream()
+        {
+            try
+            {
+                using (var image = new MagickImage(_inputStream))
+                {
+                    var geometry = new MagickGeometry
+                    {
+                        Height = _resizeToHeight,
+                        Width = _resizeToWidth,
+                        IgnoreAspectRatio = false,
+                        Greater = true
+                    };
+                    image.Resize(geometry);
+                    using (var stream = new MemoryStream())
+                    {
+                        image.Write(stream);
+                        byte[] imageByte = stream.ToArray();
+                        File.WriteAllBytes(_outputPath, imageByte);
+                    }
+                }
+                return new Tuple<bool, string>(true, $"Image saved to {_outputPath}");
+            }
+            catch (MagickException ex) // Catch Magick ExceptionErrors
+            {
+                throw ex;
+            }
+            catch (Exception ex) // Catch Exception Errors 
+            {
+                throw ex;
+            }
+        }
+
     }
 }
--- Simple Image Resize with Magick.NET/Program.cs
+++ Simple Image Resize with Magick.NET/Program.cs
@@ -9,7 +9,7 @@ namespace Simple_Image_Resize_with_Magick.NET
         static void Main(string[] args)
         {
             ImageProcess imageProcess = new ImageProcess(@"C:\Users\Daniel\Pictures\IMG_20190212_125706_0.jpg", @"C:\Users\Daniel\Pictures\newConverted.jpg");
-            var result = imageProcess.CompressImage();
+            var result = imageProcess.Compress_Save_From_FilePath();
             if(result.Item1)
             {
                 Console.WriteLine(result.Item2);
